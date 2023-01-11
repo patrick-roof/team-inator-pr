c4e4cbe087cad1b1bcab7a95018d7953318558eb
patrick <roosterruf@gmail.com>
2023-01-10 22:56:27 -0700

    edited script files, end of day commit 1-10

--- index.js
+++ index.js
@@ -5,60 +5,131 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const teamMembers = []
+
 function managerPrompt() {
+    console.log('Adding a Team Manager:');
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'managerName',
-                message: "what is the manager's name?",
+                message: "Please enter the manager's name:",
             },
             {
                 type: 'input',
                 name: 'managerID',
-                message: "what is the manager's ID?",
+                message: "Please enter the manager's ID:",
             },
             {
                 type: 'input',
                 name: 'managerEmail',
-                message: "what is the manager's Email?",
+                message: "Please enter the manager's Email:",
             },
             {
                 type: 'input',
                 name: 'officeNumber',
-                message: "what is the manager's office number?",
+                message: "Please enter the manager's office number:",
             },
         ])
         .then((answers) => {
+            console.log(answers)
             const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.officeNumber)
+            teamMembers.push(manager);
+            optionsMenu()
+        });
+};
+
+const optionsMenu = () => {
+    console.log('Menu:');
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'userMenu',
+                message: 'Please select how you would like to proceed:',
+                choices: ['add an Engineer', 'add an Intern', 'Finish']
+            }
+        ])
+        .then(chosenStep => {
+            switch (chosenStep.userMenu) {
+                case 'add an Engineer':
+                    engineerPrompt()
+                case 'add an Intern':
+                    internPrompt();
+                case 'Finish':
+                    generateSite();
+            }
         })
 }
 
 function engineerPrompt() {
+    console.log('Now adding an Engineer to your team:')
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'engineerName',
-                message: "what is the engineer's name?",
+                message: "Please enter the engineer's name:",
             },
             {
                 type: 'input',
                 name: 'engineerID',
-                message: "what is the engineer's ID?",
+                message: "Please enter the engineer's ID:",
             },
             {
                 type: 'input',
                 name: 'engineerEmail',
-                message: "what is the engineer's Email?",
+                message: "Please enter the engineer's Email:",
             },
             {
                 type: 'input',
                 name: 'engineerGitHub',
-                message: "What is the engineer's GitHub profile?"
+                message: "Please enter the engineer's GitHub username:"
             }
         ])
         .then((answers) => {
             const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub)
+            teamMembers.push(engineer);
+            userMenu();
         })
 }
+
+function internPrompt() {
+    console.log('Now adding an Intern to your team:')
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "Please enter the intern's name:",
+            },
+            {
+                type: 'input',
+                name: 'internID',
+                message: "Please enter the intern's ID:",
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: "Please enter the intern's Email:",
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: "Please enter the intern's current school:"
+            }
+        ])
+        .then((answers) => {
+            const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool)
+            teamMembers.push(intern);
+            userMenu();
+        });
+};
+
+function generateSite() {
+    console.log('Generating team info...')
+    fs.writeFile(generateHTML(teamMembers))
+}
+
+managerPrompt();
